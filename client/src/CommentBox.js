cf6d4d766b0c2e978bea94ce1b1dec3ca5243dfc
David Dinh <dmdinh22@gmail.com>
2018-05-19 00:38:20 -0700

    wire up posting comments

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -36,6 +36,29 @@ class CommentBox extends Component {
       });
   };
 
+  onChangeText = e => {
+    const newState = { ...this.state };
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  };
+
+  submitComment = e => {
+    e.preventDefault();
+    const { author, comment } = this.state;
+    if (!author || !comment) return;
+    fetch("/api/comments", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ author, comment })
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (!res.success)
+          this.setState({ error: res.error.message || res.error });
+        else this.setState({ author: "", text: "", error: null });
+      });
+  };
+
   render() {
     return (
       <div className="container">
@@ -44,7 +67,12 @@ class CommentBox extends Component {
           <CommentList data={this.state.data} />
         </div>
         <div className="form">
-          <CommentForm author={this.state.author} text={this.state.text} />
+          <CommentForm
+            author={this.state.author}
+            text={this.state.text}
+            handleChangeText={this.onChangeText}
+            handleSubmit={this.submitComment}
+          />
         </div>
         {this.state.error && <p>{this.state.error}</p>}
       </div>
